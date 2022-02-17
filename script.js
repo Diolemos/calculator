b9304eb1701fba0184ad6592b6d459f04589ace5
Dio Lemos <pedrodiogenes@outlook.com>
2022-02-17 23:26:25 +0000

    script
    Added variables currentNum, firstNum, secondNum.
    
    If the user selects an operator, the firstNum is equal to the currentnum, currentNum resets to an empty string and the operator is equal to the inner text from last button pressed

--- script.js
+++ script.js
@@ -12,23 +12,45 @@ function operate(num1, num2, op) {
 }
 // display 
 let display = document.querySelector('.display')
+display.innerText = '0'
 // buttons nodelist
 let btns = document.querySelectorAll('.btn')
-let displayText = '';
+//variable 
+let operator = ""
+let currentNum = ""
+let firstNum = ""
+let secondNum = ""
+let result = ""
 
 // event listener for the buttons
 btns.forEach(function (button) {
     button.addEventListener('click', (event) => {
+        //This replaces the zero on the calculator
+        if (display.innerText === '0') {
+            display.innerText = ''
+        }
+
+        if (button.innerText == '/' || button.innerText.toLowerCase() == 'x' || button.innerText == '-' || button.innerText == '+') {
+            firstNum = currentNum;
+            currentNum = ''
+            operator = button.innerText.trim()
+            console.log('test')
+            return true
+        }
+
         let num = button.innerText.trim();
-        displayText += num;
-        display.innerText = displayText;
+        currentNum += num;
+        display.innerText += num;
 
         if (button.innerText == 'C') {
             displayText = '';
             display.innerText = displayText;
             return true
         }
+        if (button.innerText == '+') {
+
+            return true
+        }
     })
+
 })
-
-
