9fcfb87b486a4e99ab9860c727069b7002d866ed
Dio Lemos <pedrodiogenes@outlook.com>
2022-02-18 01:38:48 +0000

    / operator and CLEAR button
    I switched the variable operator to get the result from the variables firstNum and SecondNum;
    
    I commented the return statement on line 37, So if the user presses Clear a second time, the Clear's IF statement' can be run and the display will not be empty

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function subtract() { }
 
 function multiply() { }
 
-function devide() { }
+function devide() {
+
+}
 
 function operate(num1, num2, op) {
 
@@ -26,31 +28,48 @@ let result = ""
 btns.forEach(function (button) {
     button.addEventListener('click', (event) => {
         //This replaces the zero on the calculator
+        //return true was commented so if the user presses Clear a second time
+        //The Clear statement can be run and the display will not be empty
         if (display.innerText === '0') {
             display.innerText = ''
+            currentNum = '';
+            firstNum = '';
+            // return true
         }
 
         if (button.innerText == '/' || button.innerText.toLowerCase() == 'x' || button.innerText == '-' || button.innerText == '+') {
             firstNum = currentNum;
             currentNum = ''
             operator = button.innerText.trim()
+            display.innerText += " " + button.innerText + " ";
             console.log('test')
             return true
         }
+        if (button.innerText == '=') {
+            secondNum = currentNum;
+            currentNum = ''
 
-        let num = button.innerText.trim();
+            switch (operator) {
+                case '/':
+                    result = +firstNum / +secondNum
+                    break;
+            }
+            display.innerText = result;
+            return true
+        }
+
+        let num = button.innerText;
         currentNum += num;
         display.innerText += num;
 
         if (button.innerText == 'C') {
-            displayText = '';
-            display.innerText = displayText;
-            return true
-        }
-        if (button.innerText == '+') {
+            display.innerText = '0';
+            currentNum = '';
+            firstNum = '';
 
             return true
         }
+
     })
 
 })
